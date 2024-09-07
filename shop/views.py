06d677f3db901aefeeb9bcaf5c3a1dbae80c568a
Luther Isaboke <lusaboke@gmail.com>
2024-09-07 20:25:30 +0300

    displayed mybooks on the home.html

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Book
 
 
 
 # Create your views here.
 def home(request):
+    mybooks = Book.objects.all().values()
     template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    context = {'mybooks': mybooks}
+    return HttpResponse(template.render(context,request))
